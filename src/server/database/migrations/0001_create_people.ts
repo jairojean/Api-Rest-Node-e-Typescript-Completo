d047bd71592159997f5f27640b35d4d71c00a128
jairo jean <jairojean.br@gmail.com>
2025-01-23 20:36:59 -0300

    feat: fix bug

--- src/server/database/migrations/0001_create_people.ts
+++ src/server/database/migrations/0001_create_people.ts
@@ -11,14 +11,7 @@ export async function up(knex: Knex) {
       table.string('fullName').index().notNullable();
       table.string('email').unique().notNullable();
 
-      table
-        .bigInteger('cityId')
-        .index()
-        .notNullable()
-        .references('id')
-        .inTable(ETableNames.city)
-        .onUpdate('CASCADE')
-        .onDelete('RESTRICT');
+
 
 
       table.comment('Tabela usada para armazenar peoples do sistema.');
